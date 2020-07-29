bfdaa7658999738155fd2078928e4526ce8fd610
Matej Klemen <matej.klemen1337@gmail.com>
2020-07-29 12:22:21 +0200

    Use print if running in kaggle notebook, otherwise use logging.info

--- bert.py
+++ bert.py
@@ -9,10 +9,15 @@ from transformers import BertModel
 from torch.utils.data import DataLoader, Subset
 from utils import UPOS2IDX, UFEATS2IDX, PAD
 import argparse
+import os
+
+# A bit of a hack so that `print` is used instead of `log` on kaggle since that doesn't seem to work properly there
+is_kaggle = os.path.exists("/kaggle/input")
+log_to_stdout = print if is_kaggle else logging.info
 
 logging.basicConfig(level=logging.INFO)
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-logging.info(f"Using device {DEVICE}")
+log_to_stdout(f"Using device {DEVICE}")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--train_path", type=str, default="preprocessed/train.csv")
@@ -104,7 +109,7 @@ class MorphologicalBertForSequenceClassification(nn.Module):
         self.embedders, self.poolers = nn.ModuleDict(), nn.ModuleDict()
         common_pooler = MaskedMeanPooler().to(DEVICE)
         for feature_name, emb_size in self.additional_features.items():
-            logging.info(f"Initializing embedding layer of size {emb_size} for features '{feature_name}'")
+            log_to_stdout(f"Initializing embedding layer of size {emb_size} for features '{feature_name}'")
             num_embeddings = len(UPOS2IDX) if feature_name == "upostag" else len(UFEATS2IDX[feature_name])
             padding_idx = UPOS2IDX[PAD] if feature_name == "upostag" else UFEATS2IDX[feature_name][PAD]
 
@@ -113,13 +118,13 @@ class MorphologicalBertForSequenceClassification(nn.Module):
                                                         embedding_dim=emb_size,
                                                         padding_idx=padding_idx).to(DEVICE)
             if self.pooling_type == "lstm":
-                logging.info(f"Initializing LSTM pooler with hidden state size {emb_size}")
+                log_to_stdout(f"Initializing LSTM pooler with hidden state size {emb_size}")
                 self.poolers[feature_name] = LSTMPooler(hidden_size=emb_size).to(DEVICE)
             elif self.pooling_type == "weighted":
-                logging.info(f"Initializing weighted sum pooler")
+                log_to_stdout(f"Initializing weighted sum pooler")
                 self.poolers[feature_name] = WeightedSumPooler(embedding_size=emb_size).to(DEVICE)
             else:
-                logging.info(f"Initializing mean pooler")
+                log_to_stdout(f"Initializing mean pooler")
                 self.pooling_type = "mean"
                 self.poolers[feature_name] = common_pooler
 
@@ -245,43 +250,43 @@ class BertController:
 
         t_start = time.time()
         for idx_epoch in range(num_epochs):
-            logging.info(f"Epoch#{1 + idx_epoch}/{num_epochs}")
+            log_to_stdout(f"Epoch#{1 + idx_epoch}/{num_epochs}")
             shuffled_indices = torch.randperm(len(train_dataset))
 
             num_minisets = (len(train_dataset) + self.validate_every_n_steps - 1) // self.validate_every_n_steps
             for idx_miniset in range(num_minisets):
-                logging.info(f"Miniset#{1 + idx_miniset}/{num_minisets}")
+                log_to_stdout(f"Miniset#{1 + idx_miniset}/{num_minisets}")
                 curr_subset = Subset(train_dataset, shuffled_indices[idx_miniset * self.validate_every_n_steps:
                                                                      (idx_miniset + 1) * self.validate_every_n_steps])
 
                 train_loss = self.train(curr_subset)
-                logging.info(f"Training loss: {train_loss: .4f}")
+                log_to_stdout(f"Training loss: {train_loss: .4f}")
 
                 if dev_dataset is None or len(curr_subset) < self.validate_every_n_steps // 2:
-                    logging.info(f"Skipping validation after training on a small training subset "
-                                 f"({len(curr_subset)} examples)")
+                    log_to_stdout(f"Skipping validation after training on a small training subset "
+                                  f"({len(curr_subset)} examples)")
                     continue
 
                 dev_metrics = self.validate(dev_dataset)
-                logging.info(f"Validation accuracy: {dev_metrics['accuracy']:.4f}")
+                log_to_stdout(f"Validation accuracy: {dev_metrics['accuracy']:.4f}")
                 if dev_metrics["accuracy"] > best_dev_acc:
                     best_dev_acc, rounds_no_increase = dev_metrics["accuracy"], 0
-                    logging.info(f"New best, saving checkpoint TODO")
+                    log_to_stdout(f"New best, saving checkpoint TODO")
                     # TODO: save checkpoint
                     # ...
                 else:
                     rounds_no_increase += 1
 
                 if rounds_no_increase == self.early_stopping_rounds:
-                    logging.info(f"Stopping early after no improvement for {rounds_no_increase} checks")
-                    logging.info(f"Best accuracy: {best_dev_acc:.4f}")
+                    log_to_stdout(f"Stopping early after no improvement for {rounds_no_increase} checks")
+                    log_to_stdout(f"Best accuracy: {best_dev_acc:.4f}")
                     stop_early = True
                     break
 
             if stop_early:
                 break
 
-        logging.info(f"Training took {time.time() - t_start: .3f}s")
+        log_to_stdout(f"Training took {time.time() - t_start: .3f}s")
 
 
 if __name__ == "__main__":
@@ -293,7 +298,7 @@ if __name__ == "__main__":
 
     tokenizer = BertTokenizer.from_pretrained(args.pretrained_model_name_or_path)
 
-    logging.info("Loading training dataset")
+    log_to_stdout("Loading training dataset")
     train_df = pd.read_csv(args.train_path)
     train_features = list(map(lambda features_str: json.loads(features_str), train_df["features"].values))
     train_dataset = BertDataset(sequences=train_df["content"].values,
@@ -305,7 +310,7 @@ if __name__ == "__main__":
 
     dev_df, dev_features, dev_dataset = None, None, None
     if args.dev_path:
-        logging.info("Loading validation dataset")
+        log_to_stdout("Loading validation dataset")
         dev_df = pd.read_csv(args.dev_path)
         dev_features = list(map(lambda features_str: json.loads(features_str), dev_df["features"].values))
         dev_dataset = BertDataset(sequences=dev_df["content"].values,
