a924fee44de09b64b37059cdfe7a20f1027217e8
Matej Klemen <matej.klemen1337@gmail.com>
2021-10-07 11:44:42 +0200

    Change Korean preparation script: use dev set as test set and set aside part of training set as new dev set

--- data/KOR/kor_hate/prepare_kor_hate.py
+++ data/KOR/kor_hate/prepare_kor_hate.py
@@ -3,25 +3,28 @@ import pandas as pd
 
 train_path = "labeled/train.tsv"
 dev_path = "labeled/dev.tsv"
-test_path = "test.no_label.tsv"
 
+# Test set is private: use dev set as test set
 train_df = pd.read_csv(train_path, sep="\t")[["comments", "hate"]]
-dev_df = pd.read_csv(dev_path, sep="\t")[["comments", "hate"]]
-test_df = pd.read_csv(test_path, sep="\t")
-# Test set is private
-test_df["hate"] = "none"
+test_df = pd.read_csv(dev_path, sep="\t")[["comments", "hate"]]
 
 train_df.columns = ["content", "target"]
-dev_df.columns = ["content", "target"]
 test_df.columns = ["content", "target"]
 
 # Group "offensive" and "hate" labels into one label -> test set has no labels
 train_df.at[np.logical_or(train_df["target"] == "offensive", train_df["target"] == "hate"), "target"] = "hate"
-dev_df.at[np.logical_or(dev_df["target"] == "offensive", dev_df["target"] == "hate"), "target"] = "hate"
+test_df.at[np.logical_or(test_df["target"] == "offensive", test_df["target"] == "hate"), "target"] = "hate"
 
 train_df["target"] = train_df["target"].apply(lambda str_label: {"none": 0, "hate": 1}[str_label])
-dev_df["target"] = dev_df["target"].apply(lambda str_label: {"none": 0, "hate": 1}[str_label])
 test_df["target"] = test_df["target"].apply(lambda str_label: {"none": 0, "hate": 1}[str_label])
+
+# Set aside part of training set as dev set
+indices = np.random.permutation(train_df.shape[0])
+train_indices = indices[: int(0.8 * indices.shape[0])]
+dev_indices = indices[int(0.8 * indices.shape[0]):]
+
+dev_df = train_df.iloc[dev_indices].reset_index(drop=True)
+train_df = train_df.iloc[train_indices].reset_index(drop=True)
 
 print(f"{train_df.shape[0]} train examples, \n"
       f"{dev_df.shape[0]} dev examples, \n"
